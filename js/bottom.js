e9a8ffe13b150b39ceb0d9602065cef90cc293ad
son1122 <wongsatorn.n@hotmail.com>
2022-11-14 16:02:36 +0700

    git add
    try to solve bug

--- js/bottom.js
+++ js/bottom.js
@@ -1,22 +1,31 @@
- let disk = 6
-    function initial(){
-    for(let i=0;i<disk;i++){
+ let disk = 4
+
+ function initial(){
+    for(let i=1;i<=disk;i++){
     let node = document.createElement("canvas")
         let text = document.createTextNode("Your browser does not support the HTML canvas tag.")
     node.setAttribute("id",i)
     node.setAttribute("style","border:1px solid #d3d3d3;")
-    node.setAttribute("draggable","true")
-    node.setAttribute("ondragstart","drag(event)")
     node.append(text)
-    var ctx = node.getContext("2d");
+    // node.setAttribute("draggable","true")
+    // node.setAttribute("ondragstart","drag(event)")
+
+        var ctx = node.getContext("2d");
     ctx.font = "50px Arial";
-    ctx.strokeText(i, 10, 50);
-    console.log(node)
+        ctx.textBaseline = 'middle';
+        ctx.textAlign = 'center';
+        ctx.fillText(i, 150, 80);
+
     document.querySelector("#pollOne").append(node)
 }
+    document.querySelector("#pollOne").children[0].setAttribute("draggable","true")
+     document.querySelector("#pollOne").children[0].setAttribute("ondragstart","drag(event)")
 }
-checkWin(){
-    
+ function checkWin() {
+     let numb = document.querySelector("#pollThree").children.length;
+     if(numb === disk){
+         alert("You win")
+     }
  }
-    initial()
+ initial()
 document.querySelector("#clearBtn").addEventListener("click",clear)
--- js/main.js
+++ js/main.js
@@ -2,16 +2,66 @@ function allowDrop(ev) {
     ev.preventDefault();
 }
 
-    function drag(ev) {
+function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-    function drop(ev) {
+function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    let parent = null
+    try{
+        parent = document.getElementById(ev.dataTransfer.getData("text")).parentNode
+    }catch (ev){
+
+    }
+    // if(document.getElementById(ev.dataTransfer.getData("text")).parentNode!=null){
+    //     parent = document.getElementById(ev.dataTransfer.getData("text")).parentNode
+    // }
+    if (ev.target.id === "pollOne" || ev.target.id === "pollTwo" || ev.target.id === "pollThree") {
+        console.log(ev.target.firstChild.id)
+        console.log()
+        if (ev.target.firstChild.id < ev.dataTransfer.getData("text")) {
+            console.log("I cannot")
+        } else {
+            if (document.querySelector("#" + ev.target.id).hasChildNodes()) {
+                console.log(document.querySelector("#" + ev.target.id).hasChildNodes())
+                console.log(document.querySelector("#" + ev.target.id).children)
+            }
+            console.log(ev.target.firstChild.id)
+            console.log(ev.target.id)
+            var data = ev.dataTransfer.getData("text");
+            ev.target.prepend(document.getElementById(data));
+
+            checkWin()
+        }
+    } else if (ev.target.tagName === "canvas") {
+        // var data = ev.dataTransfer.getData("text");
+        // console.log("test")
+        // console.log(document.getElementById(data).parentNode)
+        // ev.target.appendChild(document.getElementById(data).parentNode);
+        // checkWin()
+    }
+    // let parent = document.getElementById(ev.dataTransfer.getData("text")).parentNode.children[0]
+    // console.log(parent.getAttribute("id"))
+    // console.log(ev.dataTransfer.getData("text"))
+
+    if(ev.target.children[1]!=null){
+        console.log("test1")
+        ev.target.children[1].setAttribute("draggable","false")
+    }
+    if(parent!=null){
+        console.log("test2")
+        parent.children[0].setAttribute("draggable","true")
+    }
+    // try {
+    //     ev.target.children[1].setAttribute("draggable","false")
+    //     parent.children[0].setAttribute("draggable","true")
+    // }catch (e){
+    //
+    // }
 }
-function clear(){
+
+function clear() {
     while (document.querySelector("#pollOne").firstChild) {
         document.querySelector("#pollOne").removeChild(document.querySelector("#pollOne").lastChild);
     }
