a8bef6bf33bea378483bdf57de68df7dbe1a8686
son1122 <wongsatorn.n@hotmail.com>
2022-11-15 10:36:41 +0700

    clear button work

--- js/bottom.js
+++ js/bottom.js
@@ -1,11 +1,31 @@
- let disk = 3
-
+// rainbow color whell https://krazydad.com/tutorials/makecolors.php
+// rgbToHex() function https://stackoverflow.com/a/19765382
+let disk = 3
+let arrayColor =[]
+let colordisk = disk +1
+let phase = 6
+    if (phase == undefined) phase = 0;
+    center = 128;
+    width = 127;
+    frequency = Math.PI*2/colordisk;
+    for (var i = 0; i < colordisk; ++i)
+    {
+        red   = Math.sin(frequency*i+2+phase) * width + center;
+        green = Math.sin(frequency*i+0+phase) * width + center;
+        blue  = Math.sin(frequency*i+4+phase) * width + center;
+        arrayColor.push(rgbToHex(red,green,blue))
+    }
  function initial(){
+        // if(document.querySelector("#disk").value <1 ){
+        //     disk = document.querySelector("#disk").value
+        // }else {
+        //     disk =3
+        // }
     for(let i=1;i<=disk;i++){
     let node = document.createElement("canvas")
         let text = document.createTextNode("Your browser does not support the HTML canvas tag.")
     node.setAttribute("id",i)
-    node.setAttribute("style","border:1px solid #d3d3d3;")
+    // node.setAttribute("style","border:1px solid #d3d3d3;")
     node.append(text)
     node.setAttribute("draggable","false")
     node.setAttribute("ondragstart","drag(event)")
@@ -14,8 +34,10 @@
     ctx.font = "50px Arial";
         ctx.textBaseline = 'middle';
         ctx.textAlign = 'center';
+        ctx.fillStyle = arrayColor[i]
+        ctx.fillRect(0, 0, 300, 300);
+        ctx.fillStyle = "white"
         ctx.fillText(i, 150, 80);
-
     document.querySelector("#pollOne").append(node)
 }
     document.querySelector("#pollOne").children[0].setAttribute("draggable","true")
@@ -27,5 +49,12 @@
          alert("You win")
      }
  }
+function rgbToHex(red, green, blue) {
+    const rgb = (red << 16) | (green << 8) | (blue << 0);
+    return '#' + (0x1000000 + rgb).toString(16).slice(1);
+}
  initial()
 document.querySelector("#clearBtn").addEventListener("click",clear)
+document.querySelector("#diskNum").addEventListener("click",clear)
+
+
--- js/main.js
+++ js/main.js
@@ -9,8 +9,6 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     if (ev.target.id === "pollOne" || ev.target.id === "pollTwo" || ev.target.id === "pollThree") {
-        console.log(ev.target.firstChild.id)
-        console.log()
         if (ev.target.firstChild.id < ev.dataTransfer.getData("text")) {
             console.log("I cannot")
         } else {
@@ -39,14 +37,21 @@ function setDragAllPole(ev){
     }
 }
 function clear() {
-    while (document.querySelector("#pollOne").firstChild) {
-        document.querySelector("#pollOne").removeChild(document.querySelector("#pollOne").lastChild);
-    }
-    while (document.querySelector("#pollTwo").firstChild) {
-        document.querySelector("#pollTwo").removeChild(document.querySelector("#pollTwo").lastChild);
-    }
-    while (document.querySelector("#pollThree").firstChild) {
-        document.querySelector("#pollThree").removeChild(document.querySelector("#pollThree").lastChild);
-    }
-    initial()
+    // while (document.querySelector("#pollOne").firstChild) {
+    //     document.querySelector("#pollOne").removeChild(document.querySelector("#pollOne").lastChild);
+    // }
+    // while (document.querySelector("#pollTwo").firstChild) {
+    //     document.querySelector("#pollTwo").removeChild(document.querySelector("#pollTwo").lastChild);
+    // }
+    // while (document.querySelector("#pollThree").firstChild) {
+    //     document.querySelector("#pollThree").removeChild(document.querySelector("#pollThree").lastChild);
+    // }
+    document.querySelector("#main").innerHTML =""
+    document.querySelector("#main").innerHTML = '<div id="pollOne" ondrop="drop(event)" ondragover="allowDrop(event)">\n' +
+        '  </div>\n' +
+        '  <div id="pollTwo" ondrop="drop(event)" ondragover="allowDrop(event)">\n' +
+        '  </div>\n' +
+        '  <div id="pollThree" ondrop="drop(event)" ondragover="allowDrop(event)">\n' +
+        '  </div>'
+        initial()
 }
